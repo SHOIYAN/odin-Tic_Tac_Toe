c2121d0dbda737eef351872454e534ee4ff00c85
SHOIYAN <shoiyan9@gmail.com>
2025-10-29 21:52:36 +0300

    feat: add players object to get player info

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ const gameBoard = (function() {
     }
     
 })();
+
+const players = (function () {
+    const player1 = {name:'Player 1', sign: 'X'};
+    const player2 = {name:'Player 2', sign: 'O'};
+    function getName(playerNum) {
+        return playerNum === 1 ? player1.name : player2.name;
+    }
+    function getSign (playerNum) {
+        return playerNum === 1 ? player1.sign : player2.sign;
+    }
+    return {
+        getName,
+        getSign
+    }
+})();
