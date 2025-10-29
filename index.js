3405871d1addd870ad9ba5d3dcf2bf26d93783f3
SHOIYAN <shoiyan9@gmail.com>
2025-10-29 21:18:58 +0300

    feat: add gameboard object to store and update board

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const gameBoard = (function() {
+    let board = [null,null,null,null,null,null,null,null];
+    function updateBoard (index,sign){
+        board[index] = sign;
+    }
+    function getBoard () {
+        return board;
+    };
+    function reset () {
+        board.forEach((square,index,arr) => arr[index] = null);
+    }
+    return {
+        board,
+        updateBoard,
+        getBoard,
+        reset 
+    }
+    
+})();
