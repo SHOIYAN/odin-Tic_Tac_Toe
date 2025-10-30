2db7da42450510179e3b5be8d8bff649891a5be1
SHOIYAN <shoiyan9@gmail.com>
2025-10-30 20:15:15 +0300

    feat: add gameController module

--- index.js
+++ index.js
@@ -34,3 +34,43 @@ const players = (function () {
         getSign
     }
 })();
+
+const gameController = (function () {
+    let currentPlayer = 1;
+    let gameOver = false;
+    let roundCount = 0;
+    
+    function checkGame() {
+        if (gameOver) {
+            resetGame();
+        }else{
+            roundCount++;
+        }
+    }
+    function resetGame() {
+        gameBoard.reset();
+        gameOver = false;
+        roundCount = 0;
+        currentPlayer = 1;
+    }
+    function update(index){
+        gameBoard.updateBoard(index,players.getSign(currentPlayer));
+    }
+    function checkStatus(){
+        if (roundCount > 9) return;
+
+    }
+    function switchPlayer(){
+        currentPlayer = currentPlayer === 1 ? 2 : 1;
+    }
+    function playRound(index) {
+        checkGame();
+        update(index);
+        if (checkStatus()) {
+            gameOver = true;
+            return;
+        }
+        switchPlayer();
+    }
+    return { playRound, resetGame};
+})();
