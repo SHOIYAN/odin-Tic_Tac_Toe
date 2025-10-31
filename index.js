bba73b9f13a90ede381356d1603762c163a15e12
SHOIYAN <shoiyan9@gmail.com>
2025-10-31 20:40:21 +0300

    feat: add winning functionality in the gameController module

--- index.js
+++ index.js
@@ -39,38 +39,58 @@ const gameController = (function () {
     let currentPlayer = 1;
     let gameOver = false;
     let roundCount = 0;
-    
-    function checkGame() {
-        if (gameOver) {
-            resetGame();
-        }else{
-            roundCount++;
-        }
+    let winnerSign = null;
+
+    const winChances = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+    function getWinner() {
+    return winnerSign;
+    }
+    function checkWin() {
+        let board = gameBoard.getBoard();
+            for (const chance of winChances) {
+                    let [a,b,c] = chance;
+                    if (board[a] != null && board[a] === board[b] && board[a] === board[c] ){
+                        winnerSign = board[a];
+                        return winnerSign;
+                    }               
+            }
+        return false;
     }
     function resetGame() {
         gameBoard.reset();
         gameOver = false;
         roundCount = 0;
         currentPlayer = 1;
+        winnerSign = null;
     }
     function update(index){
         gameBoard.updateBoard(index,players.getSign(currentPlayer));
     }
-    function checkStatus(){
-        if (roundCount > 9) return;
-
+    function checkTie(){
+        return roundCount >= 9 ;
     }
     function switchPlayer(){
         currentPlayer = currentPlayer === 1 ? 2 : 1;
     }
     function playRound(index) {
-        checkGame();
-        update(index);
-        if (checkStatus()) {
-            gameOver = true;
-            return;
-        }
-        switchPlayer();
+            update(index);
+            roundCount++;
+            if (checkWin() || checkTie()){
+                currentPlayer = 1;
+                gameOver = true;
+                return;
+            };
+            switchPlayer();
+           
     }
-    return { playRound, resetGame};
+    return { playRound, resetGame, getWinner};
 })();
